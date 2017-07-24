c60a776e88db6039aa09cb38dd6b4b67b3a379f1
liuhui <liuhui_py@163.com>
2017-07-24 20:49:14 +0800

    Initial commit

---
+++ date-model/card.py
@@ -0,0 +1,37 @@
+__author__ = 'liuhui'
+
+
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])       #构建一个简单的类来表示纸牌
+
+class FrenchDeck:
+    '''
+    实现__getitem__和__len__这两个特殊方法
+    '''
+
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                                        for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+deck = FrenchDeck()
+
+for card in sorted(deck, key=spades_high):
+    print(card)
+
+
